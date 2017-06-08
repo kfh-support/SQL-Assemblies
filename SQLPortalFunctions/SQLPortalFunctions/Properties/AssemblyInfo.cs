808be701d1263260d81e6525f1e22e44da39d79e
Shaukat Ali <sali@kfh.co.uk>
2017-06-08 15:28:09 +0100

    8th June 2017 (1)
    Initial commit

---
+++ SQLPortalFunctions/SQLPortalFunctions/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Data.Sql;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("C# Web Functions CLR Assembly")]
+[assembly: AssemblyDescription("Used to OpenHouse to update web portals in real time")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kinleigh Folkard & Hayward")]
+[assembly: AssemblyProduct("SQLPortalFunctions")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
---
+++ SQLPortalFunctions/SQLPortalFunctions/Test Scripts/Test.sql
@@ -0,0 +1,29 @@
+﻿SELECT top 1 InputData.*, OutputResults.XMLPosted, OutputResults.XMLResponse, OutputResults.Success, OutputResults.AdditionalInfo FROM 
+(	
+	SELECT 
+		PIn.WebPortalID, 
+		PIn.PropertyReference, 
+		UpdateDate = GETDATE(), 
+		InsertionDate = CASE WHEN PIn.UpdateType = 'U' AND P.InsertionDate IS NULL THEN GETDATE() ELSE P.InsertionDate END,
+		DeletionDate = CASE WHEN PIn.UpdateType = 'D' AND P.DeletionDate IS NULL THEN GETDATE() WHEN PIn.UpdateType = 'U' THEN NULL ELSE P.DeletionDate END,
+		PIn.XMLToPost,
+		PIn.UpdateType,
+		UpdatePropertyURL,
+		RemovePropertyURL,
+		UpdatePropertyXSDPath,
+		RemovePropertyXSDPath,
+		P12CertificateLocation,
+		P12CertificatePassword
+	FROM PropertyInclude PIn
+	INNER JOIN WebPortal WP ON PIn.WebPortalID = WP.WebPortalID
+	LEFT JOIN Property P ON PIn.WebPortalID = P.WebPortalID AND PIn.PropertyReference = P.PropertyReference
+) AS InputData
+OUTER APPLY OpenHousePortals.dbo.fnWebSendXMLData
+(
+	CASE WHEN UpdateType = 'U' THEN UpdatePropertyURL ELSE RemovePropertyURL END, 
+	'POST', 'application/xml', XMLToPost, P12CertificateLocation, P12CertificatePassword, 30, 
+	CASE WHEN UpdateType = 'U' THEN UpdatePropertyXSDPath ELSE RemovePropertyXSDPath END, 
+	NULL, '/*/success[1]', 'true', 0, 
+	CASE WHEN WebPortalID = 2 THEN '/*/message[1]' ELSE '/*/*/rightmove_id[1]' END 
+) OutputResults 
+WHERE WebPortalID = 1 AND PropertyReference = 2178221
