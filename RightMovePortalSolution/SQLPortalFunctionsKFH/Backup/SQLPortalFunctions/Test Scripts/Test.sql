42c5563a6c14c0d9e9a98b578c1e335ba33cdc9a
HO\vhanbury <vhanbury@kfh.co.uk>
2018-08-03 15:24:58 +0100

    pushing new folder RightMovePortalSolution

---
+++ RightMovePortalSolution/SQLPortalFunctionsKFH/Backup/SQLPortalFunctions/Test Scripts/Test.sql
@@ -0,0 +1,12 @@
+﻿DECLARE @PropertyReference INT = (SELECT TOP 1 PropertyReference FROM Development.dbo.Property WHERE PropertyStatus = 2 AND OnWebsite = 1 AND 
+NOT EXISTS (SELECT 1 FROM Development.dbo.PropertyCoordinates WHERE PropertyReference = Property.PropertyReference))
+
+INSERT INTO Development.dbo.PropertyCoordinates VALUES(@PropertyReference, '', '')
+
+DELETE FROM OpenHousePortals.dbo.PropertyInclude
+
+UPDATE Development.dbo.Property SET EditDate = GETDATE() WHERE PropertyReference = @PropertyReference
+
+EXEC OpenHousePortals.dbo.uspWebUpdateProperties
+
+DELETE FROM Development.dbo.PropertyCoordinates WHERE PropertyReference = @PropertyReference
